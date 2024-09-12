cb557e6b9c2e3598c9efbbfb354a0a10dc25973a
Jeshan_nath <jeshannath.14@gmail.com>
2024-09-12 21:01:42 +0600

    Add files via upload

---
+++ Play with printf(prob 1-5)/problem_03.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Name:MR. x\n");
+    printf("unv: IIUC");
+    return 0;
+}
---
+++ Play with printf(prob 1-5)/problem_05.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Name:MR.Don\n");
+    printf("country:Bangladesh\n\n\n");
+    printf("contract: MRdone@mycompany.com");
+    return 0;
+}
